f48b7b6e090ccf7e3eb5b7db5dd62ee10c489268
Juan081 <85894632+Juan081@users.noreply.github.com>
2023-05-07 23:40:35 -0500

    Terminado Lab06

---
+++ ListViewExample/ListViewExample/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListViewExample
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string FamilyType { get; set; }
+        public int Limbs { get; set; }
+    }
+}
---
+++ ListViewExample/ListViewExample/ListViewGroup.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        public ListViewGroup()
+        {
+            InitializeComponent();
+
+            List<Animal> animals = new List<Animal>
+            {
+                new Animal { Name = "Pez", Limbs = 5, FamilyType = "Fish" },
+                new Animal { Name = "Gato", Limbs = 1, FamilyType = "Mammals" },
+                new Animal { Name = "Ardilla", Limbs = 7, FamilyType = "Mammals" },
+                new Animal { Name = "Loro", Limbs = 1, FamilyType = "Mammals" }
+            };
+
+            var groupedAnimal = animals.GroupBy(x => x.FamilyType)
+                .Select(g => new Grouping<string, Animal>(g.Key, g));
+
+            animalsListView.ItemsSource = groupedAnimal;
+            BindingContext = this;
+        }
+        public class Grouping<K, T> : ObservableCollection<T>
+        {
+            public K Key { get; private set; }
+            public Grouping(K key, IEnumerable<T> items)
+            {
+                Key = key;
+                foreach (var item in items)
+                    Items.Add(item);
+            }
+        }
+    }
+}
